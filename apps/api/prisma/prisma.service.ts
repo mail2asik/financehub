e075a788eae09c80337fa91620098a9188ad74a0
Mohamed Asik <mail2asik@gmail.com>
2026-08-16 11:22:17 +0000

    Fixed compatability issue for prisma client

--- apps/api/prisma/prisma.service.ts
+++ apps/api/prisma/prisma.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
-    await $connect();
+    await this.$connect();
   }
 
   async onModuleDestroy() {
-    await $disconnect();
+    await this.$disconnect();
   }
 }
